0692287a8ded6dc2432509e8db09382139d2b783
Christian Ashley <christianzashley@gmail.com>
2025-04-24 22:11:39 -0400

    Document lab2, begin automation of attributes

--- lab2.py
+++ lab2.py
@@ -6,6 +6,9 @@ from src.data_collector import WeatherReport
 from src.decision_tree import DecisionTree
 
 def data_store(file='./raw.json',operation='r',data=None,indent=None):
+  """
+  Open a file and either read and parse its JSON, or stringify JSON and write to file. Pass in indent to add pretty margins.
+  """
   with open(file, operation) as file:
     if operation == 'r':
       json_str = file.read()
@@ -15,6 +18,9 @@ def data_store(file='./raw.json',operation='r',data=None,indent=None):
 
 
 def get_training_data():
+  """
+  Read from data file or query forecast.weather.gov and parse to WeatherReport format, writing contents to file.
+  """
   try:
     return data_store()
   except OSError as ose: {
@@ -37,45 +43,72 @@ def get_training_data():
 
 
 def check_missing_attribute(attr1, attr2, comparison:callable)->bool:
+  """
+  Check if either attribute is missing before comparing with a comparison function.
+  """
   if attr1 == 'M' or attr2 == 'M': return None
   return comparison(attr1,attr2)
 
 
 def check_precip(amount:str|float,threshold=0.2)->bool:
+  """
+  Check for greater than trace amounts of precipitation.
+  """
   if amount == 'T': return False
   return check_missing_attribute(amount, threshold, lambda x,y: x > y)
 
 
-def construct_hotter_daily_data(weather_data:dict):
+def construct_hotter_daily_data(weather_data:dict)->DecisionTree:
+  """
+  Contruct a Decision Tree for checking if it will be hotter in Rochester than the previous day. 
+  """
   attributes = weather_data['attributes']
   new_attributes = (
+    'RAINED YESTERDAY IN BUF',
+    'RAINED YESTERDAY IN DTW',
+    'RAINED YESTERDAY IN ROC',
+    'SNOWED YESTERDAY IN BUF',
+    'SNOWED YESTERDAY IN DTW',
+    'SNOWED YESTERDAY IN ROC',
     'HOTTER YESTERDAY IN BUF',
     'HOTTER YESTERDAY IN DTW',
-    'RAINED YESTERDAY IN ROC',
     'HOTTER TODAY'
   )
   data = []
-  avg_temp_index = attributes.index('AVG TEMP (F)')
-  rain_index = attributes.index('WTR PCPN')
+  # 0-1, precip indices
+  # else, check_missing_attr
+  attribute_indices = [
+    attributes.index('WTR PCPN'),
+    attributes.index('SNOW'),
+    attributes.index('AVG TEMP (F)')
+  ]
+  temp_ind = attribute_indices[len(attribute_indices)-1]
+  cities = ['BUF','DTW','ROC']
 
   for day in weather_data['ROC'].keys():
+    roc_today = weather_data['ROC'][day]
     prev_day = WeatherReport.date_to_str(WeatherReport.str_to_date(day) - timedelta(days=1))
-    if weather_data['ROC'].get(prev_day, None) is not None:
-      rochester_temp_today = weather_data['ROC'][day][avg_temp_index]
-      rochester_temp_yesterday = weather_data['ROC'][prev_day][avg_temp_index]
-      buffalo_temp_yesterday = weather_data['BUF'][prev_day][avg_temp_index]
-      detroit_temp_yesterday = weather_data['DTW'][prev_day][avg_temp_index]
-      rained_yesterday = weather_data['ROC'][prev_day][rain_index]
-      greater_than = lambda x,y: x > y
-      hotter_today = check_missing_attribute(rochester_temp_today, rochester_temp_yesterday, greater_than)
-      if hotter_today is None: continue
-      data.append((
-        check_missing_attribute(buffalo_temp_yesterday, rochester_temp_yesterday, greater_than),
-        check_missing_attribute(detroit_temp_yesterday, rochester_temp_yesterday, greater_than),
-        check_precip(rained_yesterday),
-        hotter_today
-      ))
-  # print(data)
+    if weather_data['ROC'].get(day, None) is None or weather_data['ROC'].get(prev_day, None) is None:
+      continue
+    roc_prev = weather_data['ROC'][prev_day]
+    greater_than = lambda x,y: x > y
+    hotter_today = check_missing_attribute(roc_today[temp_ind],roc_prev[temp_ind],greater_than)
+    if hotter_today is None: continue
+    attrs = []
+    for i in range(len(attribute_indices)):
+      ai = attribute_indices[i]
+      roc_prev_attr = roc_prev[ai]
+      for city in cities:
+        city_prev_attr = weather_data[city][prev_day][ai]
+        if i < 2:
+          attrs.append(check_precip(city_prev_attr))
+        elif city == 'ROC' and i == len(attribute_indices) - 1:
+          attrs.append(roc_today[ai] > roc_prev_attr)
+        else:
+          attrs.append(check_missing_attribute(city_prev_attr,roc_prev_attr,greater_than))
+    data.append(tuple(attrs))
+        
+  print(data)
   print(DecisionTree(data,list(new_attributes)))
 
 
